fd1e5050464228e3997a5053617bdaad995c489f
Mohamed Nabil <medosara2011@gmail.com>
2024-01-22 19:02:30 +0200

    modify products-section

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,7 +7,18 @@ import { SiStarship } from "react-icons/si";
 import { LiaStarSolid } from "react-icons/lia";
 import { StoreContext } from '@/contexts/StoreContext';
 
-const ProductCard = () => {
+
+interface productCardProps{
+    image: string
+    rating: number
+    title: string
+    prePrice: number
+    currentPrice: number
+    ratingsNumber:number
+}
+
+
+const ProductCard = ({image,rating,title,prePrice,currentPrice,ratingsNumber}:productCardProps) => {
 
 const {cart} = useContext(StoreContext)
 
@@ -19,30 +30,30 @@ cart.addProduct()
       <div className='flex flex-col w-[18rem] '>
           
               <div className='transition-all cursor-pointer w-full aspect-square bg-mainWhite hover:bg-mainBlack/10 '>
-                  <Image src={"/fridge2.webp"} width={500} height={500} quality={100} alt='' className='w-full object-cover'/>
+                  <Image src={image} width={500} height={500} quality={100} alt='' className='w-full object-cover'/>
               </div>
 
           <div className='p-2 flex flex-col gap-10'>
               
               <div className='flex flex-col gap-3'>
                   
-                  <h1 className='text-2xl '>Samsung 470l French Door Fridge/Freezer | Bargains </h1>
+                  <h1 className='text-2xl h-[8rem] overflow-hidden'>{title} </h1>
                   <div className='flex flex-col gap-1'>
                       
                       
                       <div className='flex items-center gap-2'>
                           
 
-                          <ReactStars value={4.5} count={5} filledIcon={<LiaStarSolid />} halfIcon={<LiaStarSolid />} size={"1.3rem"} />
-                          <h1 className='text-mainBlack/50'>(22)</h1>
+                          <ReactStars value={rating} count={5} filledIcon={<LiaStarSolid />} halfIcon={<LiaStarSolid />} size={"1.3rem"} />
+                          <h1 className='text-mainBlack/50'>({ratingsNumber})</h1>
                       </div>
                       <div className='flex items-center gap-5'>
                           
                           <div className='relative '>
                               <div className='absolute top-1/2 -translate-y-1/2 w-full h-[2px] bg-black/50 -rotate-3'/>
-          <h2 className='text-2xl text-mainBlack/30 font-bold  text-center'>600$</h2>
+                              <h2 className='text-2xl text-mainBlack/30 font-bold  text-center'>{prePrice }$</h2>
                           </div>
-          <h2 className='text-2xl text-mainBlack/70 font-bold'>500$</h2>
+                          <h2 className='text-2xl text-mainBlack/70 font-bold'>{currentPrice }$</h2>
                       </div>
                   </div>
               </div>
--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -4,22 +4,23 @@ import ProductCard from './ProductCard'
 const ProductsSection = () => {
   return (
       <div className='w-full min-h-screen grid grid-cols-4 gap-10 px-28 py-0'>
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <ProductCard title='Samsung 470l French Door Fridge/Freezer | Bargains' image='/fridge2.webp' rating={4.5} ratingsNumber={25} prePrice={800} currentPrice={500}/>
+          <ProductCard title='Samsung 470l French Door Fridge/Freezer | Bargains' image='/fridge2.webp' rating={4.5} ratingsNumber={25} prePrice={800} currentPrice={500}/>
+          <ProductCard title=' Full HD TV Series A4G - Hisense Australia' image='/tv product.webp' rating={5} ratingsNumber={100} prePrice={2000} currentPrice={1800}/>
+          <ProductCard title='Samsung 470l French Door Fridge/Freezer | Bargains' image='/fridge2.webp' rating={4.5} ratingsNumber={25} prePrice={800} currentPrice={500}/>
+          <ProductCard title='Samsung 470l French Door Fridge/Freezer | Bargains' image='/fridge2.webp' rating={4.5} ratingsNumber={25} prePrice={800} currentPrice={500}/>
+          <ProductCard title='Samsung 470l French Door Fridge/Freezer | Bargains' image='/fridge2.webp' rating={4.5} ratingsNumber={25} prePrice={800} currentPrice={500}/>
+          <ProductCard title='Samsung 470l French Door Fridge/Freezer | Bargains' image='/fridge2.webp' rating={4.5} ratingsNumber={25} prePrice={800} currentPrice={500}/>
+          <ProductCard title=' Full HD TV Series A4G - Hisense Australia' image='/tv product.webp' rating={5} ratingsNumber={100} prePrice={2000} currentPrice={1800}/>
+          <ProductCard title='Samsung 470l French Door Fridge/Freezer | Bargains' image='/fridge2.webp' rating={4.5} ratingsNumber={25} prePrice={800} currentPrice={500}/>
+          <ProductCard title=' Full HD TV Series A4G - Hisense Australia' image='/tv product.webp' rating={5} ratingsNumber={100} prePrice={2000} currentPrice={1800}/>
+          <ProductCard title=' Full HD TV Series A4G - Hisense Australia' image='/tv product.webp' rating={5} ratingsNumber={100} prePrice={2000} currentPrice={1800}/>
+          <ProductCard title=' Full HD TV Series A4G - Hisense Australia' image='/tv product.webp' rating={5} ratingsNumber={100} prePrice={2000} currentPrice={1800}/>
+          <ProductCard title='Samsung 470l French Door Fridge/Freezer | Bargains' image='/fridge2.webp' rating={4.5} ratingsNumber={25} prePrice={800} currentPrice={500}/>
+          <ProductCard title=' Full HD TV Series A4G - Hisense Australia' image='/tv product.webp' rating={5} ratingsNumber={100} prePrice={2000} currentPrice={1800}/>
+          <ProductCard title=' Full HD TV Series A4G - Hisense Australia' image='/tv product.webp' rating={5} ratingsNumber={100} prePrice={2000} currentPrice={1800}/>
+          <ProductCard title=' Full HD TV Series A4G - Hisense Australia' image='/tv product.webp' rating={5} ratingsNumber={100} prePrice={2000} currentPrice={1800}/>
+       
    </div>
   )
 }

33164b73010b54cf754b74cc1378df52b6e6bf9d
Mohamed Nabil <medosara2011@gmail.com>
2024-01-23 14:43:42 +0200

    define an initial structure for user store

--- src/stores/index.tsx
+++ src/stores/index.tsx
@@ -4,6 +4,7 @@ import { CartStore } from "./cartStore";
 import { UserDropStore } from "./userDropStore";
 
 import React from 'react'
+import { userStore } from "./userStore";
 
 // cart store instance 
 
@@ -13,12 +14,16 @@ import React from 'react'
 
  const userDropInstance = new UserDropStore()
 
+
+const userStoreInstance = new userStore()
+
 // ==========================================================
 // this object is for all stores that will for all states
 
 export const store ={
     cart: cartProducts,
-    userDrop : userDropInstance
+    userDrop: userDropInstance,
+    user :userStoreInstance
 }
 
 interface indexChildren {

dbc8482e56d1a0c4b75236799ca32855cafb551b
Mohamed Nabil <medosara2011@gmail.com>
2024-03-17 12:04:26 +0200

    make the footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,70 +1,105 @@
-"use client"
-import { Divider, Image } from '@nextui-org/react'
-import React from 'react'
+"use client";
+import { Divider, Image } from "@nextui-org/react";
+import React from "react";
 
 import { TfiEmail } from "react-icons/tfi";
 import { TfiHeadphone } from "react-icons/tfi";
-import IconCard from './IconCard'
+import IconCard from "./IconCard";
 import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import Icon from './Icon';
+import Icon from "./Icon";
 
 const Footer = () => {
   return (
-      <div className='min-h-[20rem] p-5 pt-20 flex flex-col gap-16'>
-         
-          <div className='flex flex-col gap-5'>
-              
-          <Divider />
-          <div className='flex justify-between items-center '>
-              <div>
-                  
-              <h1 className='text-2xl font-bold capitalize'>We are always here to help you </h1>
-              <h1 className='text-lg font-bold capitalize text-mainBlack/50'>Reach out to us through any of these support channels</h1>
-              </div>
-              <div className='flex gap-5 items-center'>
-                  
-                 <IconCard icon={<TfiEmail />} topText='email support' mainText='store@farida.com'/>
-                 <IconCard icon={<TfiHeadphone />} topText='phone support' mainText='01089953368'/>
-                   
-              </div>
+    <div className=" pt-20 pb-16">
+      <Divider />
+      <div className="min-h-[20rem] p-5 pb-0 flex flex-col gap-5 ">
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col justify-center gap-5 items-center ">
+            <div className="flex flex-col items-center text-center">
+              <h1 className="text-xl md:text-2xl font-bold capitalize">
+                We are always here to help you{" "}
+              </h1>
+              <h1 className="text-sm md:text-lg font-bold capitalize text-mainBlack/50">
+                Reach out to us through any of these support channels
+              </h1>
+            </div>
+            <div className="flex flex-wrap gap-5 justify-center items-center">
+              <IconCard
+                icon={<TfiEmail className="text-lg md:text-xl" />}
+                topText="email support"
+                mainText="store@farida.com"
+              />
+              <IconCard
+                icon={<TfiHeadphone className="text-lg md:text-xl" />}
+                topText="phone support"
+                mainText="01089953368"
+              />
+            </div>
           </div>
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-3 justify-center items-center">
+            <div className="w-full flex gap-5 justify-center items-center">
+              <Image src="/apple.png" alt="" className="appDownload-image" />
+
+              <Image src="/google.png" alt="" className="appDownload-image" />
+            </div>
+            <div className="flex items-center gap-3">
+              <Icon
+                icon={<FaFacebookF />}
+                backColor="mainBlack"
+                color="mainWhite"
+                className="bg-mainBlack p-1"
+              />
+              <Icon
+                icon={<FaInstagram />}
+                backColor="mainBlack"
+                color="mainWhite"
+                className="bg-mainBlack text-mainWhite p-1"
+              />
+              <Icon
+                icon={<FaTwitter />}
+                backColor="mainBlack"
+                color="mainWhite"
+                className="bg-mainBlack text-mainWhite p-1"
+              />
+              <Icon
+                icon={<FaLinkedin />}
+                backColor="mainBlack"
+                color="mainWhite"
+                className="bg-mainBlack text-mainWhite p-1"
+              />
+            </div>
           </div>
 
-          <div className='flex flex-col gap-5'>
-              
-              <div className='flex justify-between items-center'>
-                  
-           <div className=' flex gap-5 justify-center items-center'>
-              
-                  <Image  src='/apple.png' alt='' className='appDownload-image'/>
-              
-              
-                  <Image src='/google.png' alt='' className='appDownload-image'/>
-              
-                  </div>
-                  <div className='flex items-center gap-3'>
-                      <Icon icon={<FaFacebookF />} backColor='mainBlack' color='mainWhite' size='2xl'/>
-                      <Icon icon={<FaInstagram />}  backColor='mainBlack' color='mainWhite' size='2xl'/>
-                      <Icon icon={<FaTwitter />}  backColor='mainBlack' color='mainWhite' size='2xl'/>
-                      <Icon icon={<FaLinkedin />}  backColor='mainBlack' color='mainWhite' size='2xl'/>
-                  </div>
-              </div>
-              <div className='flex justify-between items-center'>
-          <h1 className='text-center text-mainBlack/50'>© 2024 farida store. All Rights Reserved</h1>
-                  
-          <div className=' flex gap-5 items-center justify-center'>
-               <Image  src='/MasterCard_Logo.svg.webp' alt='' className='payment-image'/>
-               <Image  src='/Visa_Inc._logo.svg.png' alt='' className='payment-image'/>
-               <Image  src='/PayPal.svg.png' alt='' className='payment-image'/>
-               <Image  src='/cod-en.svg' alt='' className='payment-image'/>
+          <div className=" flex flex-wrap gap-3 justify-center md:justify-between items-center">
+            <h1 className="text-center text-mainBlack/50 text-sm">
+              © 2024 farida store. All Rights Reserved
+            </h1>
+
+            <div className=" flex gap-5 items-center justify-center">
+              <Image
+                src="/MasterCard_Logo.svg.webp"
+                alt=""
+                className="payment-image"
+              />
+              <Image
+                src="/Visa_Inc._logo.svg.png"
+                alt=""
+                className="payment-image"
+              />
+              <Image src="/PayPal.svg.png" alt="" className="payment-image" />
+              <Image src="/cod-en.svg" alt="" className="payment-image" />
+            </div>
           </div>
-          </div>
-          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/IconCard.tsx
+++ src/components/IconCard.tsx
@@ -1,30 +1,26 @@
-import React, { ReactElement } from 'react'
-import {AiFillAccountBook} from "react-icons/ai"
-import Icon from './Icon'
-import { IconType } from 'react-icons/lib'
-
+import React, { ReactElement } from "react";
+import { AiFillAccountBook } from "react-icons/ai";
+import Icon from "./Icon";
+import { IconType } from "react-icons/lib";
 
 interface iconCardInterface {
-    topText: string
-    mainText: string
-   icon: ReactElement<IconType>,
+  topText: string;
+  mainText: string;
+  icon: ReactElement<IconType>;
 }
 
-const IconCard = ({topText,mainText,icon}:iconCardInterface) => {
+const IconCard = ({ topText, mainText, icon }: iconCardInterface) => {
   return (
-    <div className='flex text-xl items-center gap-2'>
-                      
-          <div className='text-2xl'>
-              
-          <Icon icon={icon} hasBorder hasBorderHover borderSize='8' />
-          </div>
-          <div className='flex flex-col'>
-              
-              <h2 className='uppercase text-mainBlack/50'>{topText }</h2>
-              <h2 >{ mainText}</h2>
-          </div>
-                  </div>
-  )
-}
+    <div className="flex text-sm md:text-xl items-center gap-2">
+      <div className="text-2xl">
+        <Icon icon={icon} hasBorder={true} hasBorderHover={false} />
+      </div>
+      <div className="flex flex-col">
+        <h2 className="uppercase text-mainBlack/50">{topText}</h2>
+        <h2>{mainText}</h2>
+      </div>
+    </div>
+  );
+};
 
-export default IconCard
+export default IconCard;

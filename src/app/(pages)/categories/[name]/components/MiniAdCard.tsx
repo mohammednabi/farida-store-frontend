0b5ff19a24fa90ad20af96785bb52e41942ecaae
Mohamed Nabil <medosara2011@gmail.com>
2024-03-26 10:21:43 +0200

    make categories page responsive

--- src/app/(pages)/categories/[name]/components/MiniAdCard.tsx
+++ src/app/(pages)/categories/[name]/components/MiniAdCard.tsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from "react";
 
-interface MiniAdProps{
-    content:string
+interface MiniAdProps {
+  content: string;
 }
 
-const MiniAdCard = ({content}:MiniAdProps) => {
+const MiniAdCard = ({ content }: MiniAdProps) => {
   return (
-         <div className='bg-mainDarkBlue text-mainWhite text-3xl p-3 '>
-              
-              <div className='flex justify-center items-center p-10 border-mainWhite border-1 border-dashed'>
-                  
-              <h1 className='text-center'>{ content}</h1>
-</div>
-          </div>
-  )
-}
+    <div className="bg-mainDarkBlue text-mainWhite text-xl md:text-3xl p-3 ">
+      <div className="flex justify-center items-center p-10 border-mainWhite border-1 border-dashed">
+        <h1 className="text-center">{content}</h1>
+      </div>
+    </div>
+  );
+};
 
-export default MiniAdCard
+export default MiniAdCard;

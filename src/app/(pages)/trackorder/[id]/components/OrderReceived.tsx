e1d3fbabe29b80fe056b2c750222478aeb9d1de6
Mohamed Nabil <medosara2011@gmail.com>
2024-03-27 09:30:48 +0200

    make track order page responsive

--- src/app/(pages)/trackorder/[id]/components/OrderReceived.tsx
+++ src/app/(pages)/trackorder/[id]/components/OrderReceived.tsx
@@ -10,32 +10,34 @@ interface orderReceivedProps {
 const OrderReceived = ({ status, address }: orderReceivedProps) => {
   return (
     <div
-      className={`${status === "in progress" && `scale-110`} ${
+      className={`${status === "in progress" && `scale-105 md:scale-110`} ${
         status !== "in progress" && `opacity-50`
-      } transition-all w-full grid grid-cols-[6fr_1fr] items-center bg-mainWhite shadow-md border-2 border-mainGray border-solid p-5 rounded-md`}
+      } transition-all w-full grid grid-rows-[auto_auto] grid-cols-1 md:grid-rows-1 md:grid-cols-[6fr_1fr] items-center bg-mainWhite shadow-md border-2 border-mainGray border-solid p-5 rounded-md`}
     >
       <div className="flex flex-col gap-3">
         <div>
           <h1
-            className={`capitalize font-bold text-xl ${
+            className={`capitalize font-bold text-sm md:text-xl ${
               status === "in progress" ? `text-emerald-500` : ``
             }`}
           >
             {status}
           </h1>
-          <h2 className="capitalize font-bold text-2xl">1. order placed </h2>
+          <h2 className="capitalize font-bold text-lg md:text-2xl">
+            1. order placed{" "}
+          </h2>
         </div>
         <div>
-          <p className="capitalize font-semibold text-lg">
+          <p className="capitalize font-semibold text-xs md:text-lg">
             congratiulations your order was received successfully{" "}
           </p>
-          <p className="capitalize  text-lg">
+          <p className="capitalize  text-xs md:text-lg">
             your order will be shipped soon to:{" "}
           </p>
         </div>
-        <h3 className="text-lg font-bold">{address}</h3>
+        <h3 className="text-xs md:text-lg font-bold">{address}</h3>
       </div>
-      <div className=" flex justify-center items-center">
+      <div className="w-1/4 justify-self-center md:w-full flex justify-center items-center">
         <Image
           src="/received.png"
           alt=""

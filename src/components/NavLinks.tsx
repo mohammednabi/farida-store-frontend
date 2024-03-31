221eeb1a362019ed17f41f6dc5b6fc859c5674a9
Mohamed Nabil <medosara2011@gmail.com>
2024-03-31 13:08:18 +0200

    translating navbar links

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,21 +1,24 @@
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 import React from "react";
 
 const NavLinks2 = () => {
+  const t = useTranslations("navbar");
+
   return (
     <div className="capitalize hidden md:flex flex-1 shrink  text-sm lg:text-lg  ">
       <div className=" pl-36 flex flex-1 shrink  items-center justify-center gap-10">
         <Link href={"/"} className="nav-link">
-          home
+          {t("home")}
         </Link>
         <Link href={"/"} className="nav-link">
-          products
+          {t("products")}
         </Link>
         <Link href={"/order"} className="nav-link">
-          order
+          {t("order")}
         </Link>
         <Link href={"/contact"} className="nav-link">
-          contact
+          {t("contact")}
         </Link>
       </div>
     </div>

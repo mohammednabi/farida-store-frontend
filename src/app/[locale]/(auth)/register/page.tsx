4343994f05bd1af0427648fef12b07ad05f8026c
Mohamed Nabil <medosara2011@gmail.com>
2024-04-04 08:59:14 +0200

    translating the register page

--- src/app/[locale]/(auth)/register/page.tsx
+++ src/app/[locale]/(auth)/register/page.tsx
@@ -2,11 +2,19 @@ import React from "react";
 
 import RegisterForm from "./components/RegisterForm";
 import PageTitle from "@/components/PageTitle";
+import { unstable_setRequestLocale } from "next-intl/server";
+import { useTranslations } from "next-intl";
 
-const RegisterPage = () => {
+interface RegisterPageProps {
+  params: { locale: string };
+}
+
+const RegisterPage = ({ params: { locale } }: RegisterPageProps) => {
+  unstable_setRequestLocale(locale);
+  const t = useTranslations("registerForm");
   return (
     <div className="w-full h-auto">
-      <PageTitle title="register" />
+      <PageTitle title={t("register")} />
       <RegisterForm />
     </div>
   );

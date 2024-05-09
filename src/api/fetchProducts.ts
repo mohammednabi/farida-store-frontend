f40e8656f58d2c0b7fc2bcbfc32a90e3cbd0e45b
Mohamed Nabil <medosara2011@gmail.com>
2024-05-09 10:35:00 +0300

    deleteing api folder

--- src/api/fetchProducts.ts
+++
@@ -1,7 +0,0 @@
-
-
-import axios from "axios"
-
-export const fetchProducts = ():Promise<any>=>{
-    return  axios.get('https://fakestoreapi.com/products').then((res)=>res.data)
-}
--- src/api/fetchSingleProduct.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios"
-
-export const fetchSingleProduct = (id:string):Promise<any>=>{
-    return  axios.get(`https://fakestoreapi.com/products/${id}`).then((res)=>res.data)
-}
